c31057e766d3eb2898b4fc6697a4b9e150426f04
rikray1208 <rostipotapov@gmail.com>
2024-05-28 14:39:51 +0200

    utils static

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -1,17 +1,15 @@
-import { IUtils } from "../interfaces"
-
-export class Utils implements IUtils {
-  public async sleepFn(seconds: number) {
+export class Utils {
+  static async sleepFn(seconds: number) {
     await new Promise((resolve) => setTimeout(resolve, seconds * 1000))
   }
 
-  public getDeadLine(minutes: number) {
+  static getDeadLine(minutes: number) {
     const now = Date.now()
     const milliseconds = minutes * 60 * 1000
     return Math.floor((now + milliseconds) / 1000)
   }
 
-  public increaseNumber(value: number, percentage: number) {
+  static increaseNumber(value: number, percentage: number) {
     return Math.round(value + (value * (percentage / 100)))
   }
 }
